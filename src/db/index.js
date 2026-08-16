e91d2ff613a932298a2107fb45025d3d37a3e2ef
Mganeshdatta <mganeshdatta07@gmail.com>
2026-08-16 17:59:53 +0530

    Connected to DataBase

---
+++ src/db/index.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import {DB_NAME} from '../constants.js';
+
+import dns from "dns";
+
+dns.setServers(['8.8.8.8', '8.8.4.4']);
+
+const connectDB = async () =>{
+    try{
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`\n DB Connected !! DB Host: ${connectionInstance.connection.host}`);
+    }catch(error){
+        console.log("DB ERROR: ",error);
+        process.exit(1);
+    }
+}
+
+export default connectDB
